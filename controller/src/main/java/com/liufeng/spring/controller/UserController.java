bcd0efbca61228fd70dc3e37ef9c8c90dda17a8b
liufeng <1021563889@qq.com>
2017-07-18 01:42:37 -0700

    tijiaoxinban

--- controller/src/main/java/com/liufeng/spring/controller/UserController.java
+++ controller/src/main/java/com/liufeng/spring/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.liufeng.spring.controller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.liufeng.spring.interfaces.IUserService;
@@ -38,4 +41,30 @@ public class UserController {
 	 public int del(Long id) {
 		return userService.del(id);
 	 }
+	
+	 @RequestMapping("/tile/{z}/{x}/{y}.png")
+	 public byte[] getTile(@PathVariable int z,@PathVariable int x,@PathVariable int y,HttpServletResponse response){
+		 response.setContentType("image/png");
+		 String path = "/home/liufeng/workspace/testmapnik/output/"+z+"/"+x+"_"+y+".png";
+	     return getContent(path);
+	 }
+	 
+	 private byte[] getContent(String path){
+		 File file = new File(path);
+		 if(!file.exists()||file.isDirectory())
+			 return null;
+		 byte[] content = null;
+		 try{
+			 BufferedInputStream instream = new BufferedInputStream(new FileInputStream(file));
+			 int len  =instream.available();
+			 content = new byte[len];
+			 instream.read(content);
+			 instream.close();
+			 return content;		 
+		 }catch(IOException e){
+			 e.printStackTrace();
+			 throw new RuntimeException(e);
+		 }
+	 }
+
 }
